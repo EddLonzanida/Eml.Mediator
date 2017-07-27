523dfcbc5c496f8c425db9690190f08142ffee12
Eddie Lonzanida <eddielonzanida@hotmail.com>
2017-07-28 02:48:20 +0800

    Updated AssemblyInfo

--- Eml.Mediator/Properties/AssemblyInfo.cs
+++ Eml.Mediator/Properties/AssemblyInfo.cs
@@ -1,14 +1,13 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Eml.Mediator")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Implement Request-Response pattern using MEF2")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Eml Solutions Team")]
 [assembly: AssemblyProduct("Eml.Mediator")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -21,16 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d15aa7e2-eb5f-4746-805f-245debe3e20d")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
