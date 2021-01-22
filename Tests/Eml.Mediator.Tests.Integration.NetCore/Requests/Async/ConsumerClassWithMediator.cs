b41f8c4338b1aeb702b90e71b9961d29691ab6fe
Edd Lonzanida <eddielonzanida@hotmail.com>
2021-01-22 15:46:02 +0800

    Added Sample Classes

---
+++ Tests/Eml.Mediator.Tests.Integration.NetCore/Requests/Async/ConsumerClassWithMediator.cs
@@ -0,0 +1,28 @@
+﻿using Eml.Mediator.Contracts;
+using Eml.Mediator.Tests.Common.Classes;
+using Eml.Mediator.Tests.Common.Requests;
+using Eml.Mediator.Tests.Common.Responses;
+using System;
+using System.Threading.Tasks;
+
+namespace Eml.Mediator.Tests.Integration.NetCore.Requests.Async
+{
+    public class ConsumerClassWithMediator : IDiDiscoverableTransient
+    {
+        private readonly IMediator mediator;
+
+        public ConsumerClassWithMediator(IMediator mediator)        //<-Inject IMediator
+        {
+            this.mediator = mediator;
+        }
+
+        public async Task<TestResponse> DoSomething()
+        {
+            var request = new TestAsyncRequest(Guid.NewGuid());     //<-Request
+
+            var response = await mediator.ExecuteAsync(request);    //<-Execute
+
+            return response;                                        //<-Return Value
+        }
+    }
+}
---
+++ Tests/Eml.Mediator.Tests.Integration.NetCore/Requests/Async/ConsumerClassWithoutMediator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Eml.Mediator.Contracts;
+using Eml.Mediator.Tests.Common.Classes;
+using Eml.Mediator.Tests.Common.Requests;
+using Eml.Mediator.Tests.Common.Responses;
+
+namespace Eml.Mediator.Tests.Integration.NetCore.Requests.Async
+{
+    public class ConsumerClassWithoutMediator : IDiDiscoverableTransient
+    {
+        private readonly IRequestAsyncEngine<TestAsyncRequest, TestResponse> engine;
+
+        public ConsumerClassWithoutMediator(IRequestAsyncEngine<TestAsyncRequest, TestResponse> engine)     //<-Normal dependency injection
+        {
+            this.engine = engine;
+        }
+
+        public async Task<TestResponse> DoSomething()
+        {
+            var request = new TestAsyncRequest(Guid.NewGuid());     //<-Request
+
+            var response = await engine.ExecuteAsync(request);      //<-Execute
+
+            return response;                                        //<-Return Value
+        }
+    }
+}
